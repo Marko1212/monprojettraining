85ff91cc3166001b1f03cca96765734a8e56739a
Marko1212 <shihonage7@gmail.com>
2020-05-16 22:28:37 +0200

    changes

---
+++ src/app/admin/fake-session-item-service.spec.ts
@@ -0,0 +1,18 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { FakeSessionItemService } from './fake-session-item-service';
+
+describe('FakeSessionItemService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [FakeSessionItemService],
+    });
+  });
+
+  it('should be created', inject(
+    [FakeSessionItemService],
+    (service: FakeSessionItemService) => {
+      expect(service).toBeTruthy();
+    }
+  ));
+});
--- src/app/admin/session-edit-form/session-edit-form.component.ts
+++ src/app/admin/session-edit-form/session-edit-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+/* import { Component, OnInit } from '@angular/core';
 import { Session } from '../session';
 import { FakeSessionItemService } from '../fake-session-item-service';
 
@@ -15,6 +15,37 @@ export class SessionEditFormComponent implements OnInit {
 
   ngOnInit() {
     this.session = this.sessionItemService.getSession(1);
+  }
+  editSession(sessionItem) {
+    console.log(sessionItem);
+  }
+} */
+
+import { Component, OnInit } from '@angular/core';
+import { Session } from '../session';
+import { FakeSessionItemService } from '../fake-session-item-service';
+import { ActivatedRoute } from '@angular/router';
+@Component({
+  selector: 'app-session-edit-form',
+  templateUrl: './session-edit-form.component.html',
+  styleUrls: ['./session-edit-form.component.css'],
+})
+export class SessionEditFormComponent implements OnInit {
+  id: number;
+  private sub: any;
+  session: Session;
+  tracks = ['MEAN', 'Angular', 'NodeJS', 'Android', 'Swift', 'Xamarin'];
+  constructor(
+    private route: ActivatedRoute,
+    private sessionItemService: FakeSessionItemService
+  ) {}
+  ngOnInit() {
+    //this.session = this.sessionItemService.getSession(1);
+    this.sub = this.route.params.subscribe((params) => {
+      this.id = +params['id']; // (+) converts string 'id' to a number
+    });
+    console.log('Session ID ' + this.id.toString());
+    this.session = this.sessionItemService.getSession(this.id);
   }
   editSession(sessionItem) {
     console.log(sessionItem);
